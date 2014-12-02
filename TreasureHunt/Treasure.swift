024f6255777d83f4c2dcc3a5dd289d27d280edaa
liz rush <liz@lizmrush.com>
2014-12-02 09:24:40 -0800

    Add Treasure class

---
+++ TreasureHunt/Treasure.swift
@@ -0,0 +1,15 @@
+//
+//  Treasure.swift
+//  TreasureHunt
+//
+//  Created by liz on 2/12/14.
+//  Copyright (c) 2014 Razeware. All rights reserved.
+//
+
+import Foundation
+
+class Treasure(what: String, latitude: Double, longitude: Double) {
+    self.what       = what
+    self.latitude   = latitude
+    self.longitude  = longitude
+}
