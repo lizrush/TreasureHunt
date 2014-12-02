5764a35e74c138145b3f00c492ed86c5df42e745
liz rush <liz@lizmrush.com>
2014-12-02 09:45:01 -0800

    Add Treasure subclasses

--- TreasureHunt/Treasure.swift
+++ TreasureHunt/Treasure.swift
@@ -25,3 +25,35 @@ class Treasure: NSObject {
         self.init(what: what, location: location)
     }
 }
+
+class HistoryTreasue: Treasure {
+    let year: Int
+    
+    init(what: String, year: Int, latitude: Double, longitude: Double)
+    {
+        self.year = year
+        let location = GeoLocation(latitude: latitude, longitude: longitude)
+        super.init(what: what, location: location)
+    }
+}
+
+class FactTreasure: Treasure {
+    let fact: String
+    
+    init(what: String, fact: String, latitude: Double, longitude: Double)
+    {
+        self.fact = fact
+        let location = GeoLocation(latitude: latitude, longitude: longitude)
+        super.init(what: what, location: location)
+    }
+}
+
+class HQTreasure: Treasure {
+    let company: String
+    
+    init(company: String, latitude: Double, longitude: Double) {
+        self.company = company
+        let location = GeoLocation(latitude: latitude, longitude: longitude)
+        super.init(what: company + " headquarters", location: location)
+    }
+}
